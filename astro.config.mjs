a3363a09fe1a4b2e0f97cffcfffeae26b1d069ce
Orest Tokovenko <hi@oresttokovenko.com>
2025-02-26 17:24:34 -0700

    adding footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import mdx from "@astrojs/mdx";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 
   vite: {
     plugins: [tailwindcss()],
