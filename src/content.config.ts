ad255dd2b49abc3c96e540a05a1efa9a9cf960b1
Orest Tokovenko <hi@oresttokovenko.com>
2026-02-11 17:20:33 -0700

    updating og image

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
     publishDate: z.coerce.date(),
     description: z.string(),
     tags: z.array(z.string()).default([]),
+    ogImage: z.string().optional(),
     wip: z.boolean().optional(),
   }),
 });
